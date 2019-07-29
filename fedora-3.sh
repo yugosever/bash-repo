af5ee104cca1c505a785801705a4ba1315ba7c05
vl <vl@example.com>
2019-07-29 14:21:53 +0300

    fourth commit

--- fedora-3.sh
+++ fedora-3.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "This script find vms ip address and connect to ssh server"
 fedora_3_ip=`virsh net-dhcp-leases default | grep fedora-3 | awk '{print $5}' | cut -d/ -f1`
 ssh vl@$fedora_3_ip
+
