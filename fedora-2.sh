72f233f4ba3cbde8cfed1241caf51d0c4276b9cb
vl <vl@example.com>
2019-07-29 10:38:11 +0300

    add echo to fedora-2.sh

--- fedora-2.sh
+++ fedora-2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "This porgram finds vms ip address and after that make ssh connection"
 fedora_2_ip=`virsh net-dhcp-leases default | grep fedora-2 | awk '{print $5}' | cut -d/ -f1`
 ssh vl@$fedora_2_ip
